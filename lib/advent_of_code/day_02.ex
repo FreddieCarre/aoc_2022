6ca103adbe8170aec6dc3bd310148ace97f4d384
Freddie Carre <freddie@lapse.app>
2022-12-02 08:56:09 +0000

    day 2 - using char points

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -5,79 +5,32 @@ defmodule AdventOfCode.Day02 do
     |> Enum.map(fn round -> String.split(round, " ", trim: true) end)
   end
 
-  def score_round(r) do
-    score = fn
-      ["A", "X"] -> 4
-      ["B", "X"] -> 1
-      ["C", "X"] -> 7
-      ["A", "Y"] -> 8
-      ["B", "Y"] -> 5
-      ["C", "Y"] -> 2
-      ["A", "Z"] -> 3
-      ["B", "Z"] -> 9
-      ["C", "Z"] -> 6
-      _ -> 0
+  defp score({theirs, mine}) do
+    mine + 1 + case Integer.mod(mine - theirs, 3) do
+      0 -> 3
+      1 -> 6
+      2 -> 0
     end
-
-    score.(r)
   end
 
   def part1(args) do
     args
-    |> parse
-    |> Enum.map(&score_round/1)
-    |> Enum.sum()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn <<theirs, " ", mine>> -> {theirs - ?A, mine - ?X} end)
+    |> Enum.map(&score/1)
+    |> Enum.sum
   end
 
-  def get_draw(t) do
-    draw = fn
-      "A" -> "X"
-      "B" -> "Y"
-      "C" -> "Z"
-      _ -> ""
-    end
-
-    draw.(t)
-  end
-
-  def get_win(t) do
-    win = fn
-      "A" -> "Y"
-      "B" -> "Z"
-      "C" -> "X"
-      _ -> ""
-    end
-
-    win.(t)
-  end
-
-  def get_loss(t) do
-    win = fn
-      "A" -> "Z"
-      "B" -> "X"
-      "C" -> "Y"
-      _ -> ""
-    end
-
-    win.(t)
-  end
-
-  def get_throw(r) do
-   get = fn
-      [opp, "X"] -> get_loss(opp)
-      [opp, "Y"] -> get_draw(opp)
-      [opp, "Z"] -> get_win(opp)
-      _-> 0
-    end
-
-    get.(r)
+  defp choose_move({theirs, outcome}) do
+    {theirs, Integer.mod(theirs + (outcome - ?Y), 3)}
   end
 
   def part2(args) do
     args
-    |> parse
-    |> Enum.map(fn r = [opp, _] -> [opp, get_throw(r)] end)
-    |> Enum.map(&score_round/1)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn <<theirs, " ", mine>> -> {theirs - ?A, mine} end)
+    |> Enum.map(&choose_move/1)
+    |> Enum.map(&score/1)
     |> Enum.sum
   end
 end
