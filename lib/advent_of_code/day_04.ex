101602ba7ef1f517098447ebce8aa34018b30cae
Freddie Carre <freddie@lapse.app>
2022-12-04 15:40:06 +0000

    day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day04 do
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn pair -> String.split(pair, ",", trim: true) end)
+    |> Enum.map(fn pair ->
+      pair
+      |> Enum.map(fn e ->
+        e
+        |> String.split("-", trim: true)
+        |> Enum.map(fn s ->
+          {n, _} = Integer.parse(s, 10)
+          n
+        end)
+      end)
+    end)
+  end
+
   def part1(args) do
+    args
+    |> parse()
+    |> Enum.filter(fn [[a1, a2], [b1, b2]] ->
+      (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2)
+    end)
+    |> Enum.count()
   end
 
   def part2(args) do
+    args
+    |> parse()
+    |> Enum.filter(fn [[a1, a2], [b1, b2]] ->
+      (a1 >= b1 && a1 <= b2) ||
+        (a2 >= b1 && a2 <= b2) ||
+        (b1 >= a1 && b1 <= a2) ||
+        (b2 >= a1 && b2 <= a2)
+    end)
+    |> Enum.count()
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,18 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
     result = part1(input)
 
-    assert result
+    assert result == 2
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
     result = part2(input)
 
-    assert result
+    assert result == 4
   end
 end
